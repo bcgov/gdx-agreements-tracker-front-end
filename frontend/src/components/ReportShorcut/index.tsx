f2b4047d980857d63007018b0d51ab553b89efa3
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-04-11 10:06:27 -0700

    added error handler in the frontend for now row selected for report

--- frontend/src/components/ReportShorcut/index.tsx
+++ frontend/src/components/ReportShorcut/index.tsx
@@ -2,6 +2,8 @@ import { Button } from "@mui/material";
 import { apiAxios } from "utils";
 import { handleReportExport } from "utils/handleReportExport";
 import SummarizeIcon from "@mui/icons-material/Summarize";
+import { NotificationSnackBar } from "components/NotificationSnackbar";
+import { useSnackbar } from "hooks/useSnackbar";
 
 export const ReportShorcut = ({
   config,
@@ -10,6 +12,8 @@ export const ReportShorcut = ({
   config: { [key: string]: string | null };
   currentRowApiUrl: string;
 }) => {
+  const { handleSnackbar, snackbarOpen } = useSnackbar();
+
   const getCurrentRowData = async () => {
     return await apiAxios()
       .get(currentRowApiUrl)
@@ -17,20 +21,34 @@ export const ReportShorcut = ({
         return currentRow;
       });
   };
+
   const generateReport = async () => {
-    await getCurrentRowData().then((currentRowData) => {
-      handleReportExport({ ...currentRowData.data.data, ...config });
-    });
+    if (currentRowApiUrl.includes("undefined")) {
+      handleSnackbar(true);
+      return;
+    } else {
+      await getCurrentRowData().then((currentRowData) => {
+        handleReportExport({ ...currentRowData.data.data, ...config });
+      });
+    }
   };
 
   return (
-    <Button
-      onClick={generateReport}
-      color="success"
-      variant="contained"
-      endIcon={<SummarizeIcon />}
-    >
-      {config.buttonText}
-    </Button>
+    <>
+      <Button
+        onClick={generateReport}
+        color="success"
+        variant="contained"
+        endIcon={<SummarizeIcon />}
+      >
+        {config.buttonText}
+      </Button>
+      <NotificationSnackBar
+        snackbarMessage={"No row selected to run the report. Please select a row and try again."}
+        snackbarOpen={snackbarOpen}
+        snackbarType={"error"}
+        handleSnackbar={handleSnackbar}
+      />
+    </>
   );
 };
