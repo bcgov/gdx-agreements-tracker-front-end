614c9b9d65592ce6a4154bea126e2c2d491135cb
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-04-11 14:25:26 -0700

    updated width client coding section

--- frontend/src/pages/Projects/Project/ProjectDetails/ClientCodingSection/FormConfig.ts
+++ frontend/src/pages/Projects/Project/ProjectDetails/ClientCodingSection/FormConfig.ts
@@ -9,47 +9,47 @@ export const FormConfig = (query: UseQueryResult<AxiosResponse, unknown>) => {
     ? []
     : [
         {
-          width: "half",
+          width: "full",
           title: "Program Area",
           value: query?.data?.data?.data?.program_area,
         },
         {
-          width: "half",
+          width: "full",
           title: "Client",
           value: query?.data?.data?.data?.client,
         },
         {
-          width: "half",
+          width: "full",
           title: "Responsibility Centre",
           value: query?.data?.data?.data?.responsibility_centre,
         },
         {
-          width: "half",
+          width: "full",
           title: "Service Line",
           value: query?.data?.data?.data?.service_line,
         },
         {
-          width: "half",
+          width: "full",
           title: "STOB",
           value: query?.data?.data?.data?.stob,
         },
         {
-          width: "half",
+          width: "full",
           title: "Project Code",
           value: query?.data?.data?.data?.project_code,
         },
         {
-          width: "half",
+          width: "full",
           title: "Financial Contact",
           value: query?.data?.data?.data?.contact_id?.label,
         },
         {
-          width: "half",
+          width: "full",
           title: "Expense Authority Name",
           value: query?.data?.data?.data?.expense_authority_name,
         },
         {
-          width: "half",
+          width: "full",
           title: "Client Amount",
           value: query?.data?.data?.data?.client_amount,
         },
@@ -57,43 +57,43 @@ export const FormConfig = (query: UseQueryResult<AxiosResponse, unknown>) => {
 
   const editFields: IEditField[] = [
     {
-      width: "half",
+      width: "full",
       fieldLabel: "Program Area",
       fieldName: "program_area",
       fieldType: "singleText",
     },
     {
-      width: "half",
+      width: "full",
       fieldLabel: "Client",
       fieldName: "client",
       fieldType: "singleText",
     },
     {
-      width: "half",
+      width: "full",
       fieldLabel: "Responsibility Centre",
       fieldName: "responsibility_centre",
       fieldType: "singleText",
     },
     {
-      width: "half",
+      width: "full",
       fieldLabel: "Service Line",
       fieldName: "service_line",
       fieldType: "singleText",
     },
     {
-      width: "half",
+      width: "full",
       fieldLabel: "STOB",
       fieldName: "stob",
       fieldType: "singleText",
     },
     {
-      width: "half",
+      width: "full",
       fieldLabel: "Project Code",
       fieldName: "project_code",
       fieldType: "singleText",
     },
     {
-      width: "half",
+      width: "full",
       fieldLabel: "Financial Contact",
       fieldName: "contact_id",
       fieldType: "select",
@@ -101,13 +101,13 @@ export const FormConfig = (query: UseQueryResult<AxiosResponse, unknown>) => {
       required: true,
     },
     {
-      width: "half",
+      width: "full",
       fieldLabel: "Expense Authority Name",
       fieldName: "expense_authority_name",
       fieldType: "singleText",
     },
     {
-      width: "half",
+      width: "full",
       fieldLabel: "Client Amount",
       fieldName: "client_amount",
       fieldType: "money",
