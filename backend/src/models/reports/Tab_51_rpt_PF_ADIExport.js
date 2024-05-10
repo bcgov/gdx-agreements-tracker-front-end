568231cb101f3c20861117cb1a85deb0812c655a
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-05-10 15:14:58 -0700

    fixed report for output (#729)

--- backend/src/models/reports/Tab_51_rpt_PF_ADIExport.js
+++ backend/src/models/reports/Tab_51_rpt_PF_ADIExport.js
@@ -4,143 +4,161 @@ const log = require("../../facilities/logging")(module.filename);
 
 // query body template
 const subquery = `
-  WITH
-  project_budget_normalized AS (
-      SELECT  id,
-              project_deliverable_id,
-              fiscal,
-              client_coding_id,
-              contract_id,
-              notes,
-              detail_amount,
-              recovery_area,
-              resource_type,
-              stob,
-              1 QUARTER,
-              q1_recovered recovered,
-              q1_amount amount
-      FROM    project_budget
-      UNION ALL
-      SELECT  id,
-              project_deliverable_id,
-              fiscal,
-              client_coding_id,
-              contract_id,
-              notes,
-              detail_amount,
-              recovery_area,
-              resource_type,
-              stob,
-              2 QUARTER,
-              q2_recovered recovered,
-              q2_amount amount
-      FROM    project_budget
-      UNION ALL
-      SELECT  id,
-              project_deliverable_id,
-              fiscal,
-              client_coding_id,
-              contract_id,
-              notes,
-              detail_amount,
-              recovery_area,
-              resource_type,
-              stob,
-              3 QUARTER,
-              q3_recovered recovered,
-              q3_amount amount
-      FROM    project_budget
-      UNION ALL
-      SELECT  id,
-              project_deliverable_id,
-              fiscal,
-              client_coding_id,
-              contract_id,
-              notes,
-              detail_amount,
-              recovery_area,
-              resource_type,
-              stob,
-              4 QUARTER,
-              q4_recovered recovered,
-              q4_amount amount
-      FROM    project_budget
-  )
-  SELECT  p.id project_id,
-          pb.fiscal,
-          fy.fiscal_year,
+WITH project_budget_normalized
+     AS (SELECT id,
+                project_deliverable_id,
+                fiscal,
+                client_coding_id,
+                contract_id,
+                notes,
+                detail_amount,
+                recovery_area,
+                resource_type,
+                stob,
+                1            QUARTER,
+                q1_recovered recovered,
+                q1_amount    amount
+         FROM   data.project_budget prb
+         UNION ALL
+         SELECT id,
+                project_deliverable_id,
+                fiscal,
+                client_coding_id,
+                contract_id,
+                notes,
+                detail_amount,
+                recovery_area,
+                resource_type,
+                stob,
+                2            QUARTER,
+                q2_recovered recovered,
+                q2_amount    amount
+         FROM   data.project_budget prb
+         UNION ALL
+         SELECT id,
+                project_deliverable_id,
+                fiscal,
+                client_coding_id,
+                contract_id,
+                notes,
+                detail_amount,
+                recovery_area,
+                resource_type,
+                stob,
+                3            QUARTER,
+                q3_recovered recovered,
+                q3_amount    amount
+         FROM   data.project_budget prb
+         UNION ALL
+         SELECT id,
+                project_deliverable_id,
+                fiscal,
+                client_coding_id,
+                contract_id,
+                notes,
+                detail_amount,
+                recovery_area,
+                resource_type,
+                stob,
+                4            QUARTER,
+                q4_recovered recovered,
+                q4_amount    amount
+         FROM   data.project_budget prb)
+SELECT p.grp,
+       cc.client,
+       cc.responsibility_centre,
+       cc.expense_authority_name,
+       p.id                           project_id,
+       pb.fiscal,
+       fy.fiscal_year,
+       pb.quarter,
+       pb.recovery_area,
+       po.portfolio_abbrev            portfolio,
+       To_char(DATE ('20'
+                     || LEFT(fy.fiscal_year, 2)
+                     || '0401') + ( pb.quarter - 1
+                                    || ' months' ) :: interval - ( 1
+                                                                   || 'day' ) ::
+               interval, 'DD-Mon-YY') AS "accounting_date",
+       'GDXProjectBilling_'
+       || fy.fiscal_year
+       || '_'
+       || CAST(pb.quarter AS TEXT)    "journal_desc",
+       CASE
+         WHEN p.grp IS NULL THEN cc.expense_authority_name
+         ELSE po.expense_authority
+       END                            expense_authority_name,
+       CASE
+         WHEN p.grp IS NULL THEN cc.client
+         ELSE po.client
+       END                            cl,
+       CASE
+         WHEN p.grp IS NULL THEN cc.responsibility_centre
+         ELSE po.responsibility
+       END                            rsp,
+       CASE
+         WHEN p.grp IS NULL THEN cc.service_line
+         ELSE po.service_line
+       END                            srvc,
+       CASE
+         WHEN p.grp IS NULL THEN cc.stob
+         ELSE pb.stob
+       END                            stob,
+       CASE
+         WHEN p.grp IS NULL THEN cc.project_code
+         ELSE po.cas_project_code
+       END                            proj,
+       '000000'                       loc,
+       '0000'                         fut,
+       CASE
+         WHEN p.grp IS NULL THEN Sum(amount)
+         ELSE NULL
+       END                            debit,
+       CASE
+         WHEN p.grp IS NOT NULL THEN Sum(amount)
+         ELSE NULL
+       END                            credit,
+       'Q'
+       || CAST(pb.quarter AS TEXT)
+       || ' '
+       || p.project_number
+       || ' '
+       || p.project_name              "line_desc"
+FROM   project_budget_normalized pb
+       INNER JOIN data.project_deliverable pd
+               ON pb.project_deliverable_id = pd.id
+       INNER JOIN data.fiscal_year fy
+               ON pb.fiscal = fy.id
+       INNER JOIN data.portfolio po
+               ON pb.recovery_area = po.id
+       INNER JOIN data.client_coding cc
+               ON pb.client_coding_id = cc.id
+       INNER JOIN (SELECT *,
+                          id grp
+                   FROM   data.project) p
+               ON cc.project_id = p.id
+WHERE  pb.recovered = true
+GROUP  BY grouping sets ( ( pb.fiscal, fy.fiscal_year, pb.quarter,
+                            pb.recovery_area,
+              po.portfolio_abbrev, po.expense_authority, cc.client,
+                                        cc.responsibility_centre,
+              cc.service_line, cc.stob, cc.project_code, po.client,
+              po.responsibility, po.service_line, pb.stob, po.cas_project_code,
+              p.project_number, p.project_name, p.id, p.grp ), (
+            pb.fiscal, fy.fiscal_year, pb.quarter, pb.recovery_area,
+            po.portfolio_abbrev, po.expense_authority, cc.client,
+            cc.responsibility_centre,
+            cc.service_line, cc.stob, cc.project_code, p.project_number,
+            p.project_name, p.id, cc.expense_authority_name ) )
+ORDER  BY fy.fiscal_year,
           pb.quarter,
-          pb.recovery_area,
-          po.portfolio_abbrev portfolio,
-          NULL category,
-          to_char(date ('20' || left( fy.fiscal_year, 2 ) || '0401') +  (pb.quarter - 1 || ' months')::interval
-          - (1 || 'day')::interval, 'DD-Mon-YY') as "accounting_date",
-          NULL period,
-          NULL  "batch_name",
-          NULL "batch_desc",
-          NULL "journal_name",
-          'GDXProjectBilling_' || fy.fiscal_year || '_' || cast( pb.quarter AS text ) "journal_desc",
-          NULL preparer,
-          po.expense_authority "expense_authority_name",
-          CASE WHEN p.grp IS NULL THEN cc.client ELSE po.client END cl,
-          CASE WHEN p.grp IS NULL THEN cc.responsibility_centre ELSE po.responsibility END rsp,
-          CASE WHEN p.grp IS NULL THEN cc.service_line ELSE po.service_line END srvc,
-          CASE WHEN p.grp IS NULL THEN cc.stob ELSE pb.stob END stob,
-          CASE WHEN p.grp IS NULL THEN cc.project_code ELSE po.cas_project_code END proj,
-          '000000' loc,
-          '0000' fut,
-          CASE WHEN p.grp IS NULL THEN sum( amount ) ELSE NULL END debit,
-          CASE WHEN p.grp IS NOT NULL THEN sum( amount ) ELSE NULL END credit,
-          'Q' || cast( pb.quarter AS text ) || ' ' || p.project_number || ' ' || p.project_name "line_desc"
-  FROM    project_budget_normalized pb
-          INNER JOIN project_deliverable pd ON pb.project_deliverable_id = pd.id
-          INNER JOIN fiscal_year fy ON pb.fiscal = fy.id
-          INNER JOIN portfolio po ON pb.recovery_area = po.id
-          INNER JOIN client_coding cc ON pb.client_coding_id = cc.id
-          INNER JOIN ( SELECT *, id grp FROM project ) p ON cc.project_id = p.id
-  WHERE   pb.recovered = TRUE
-  GROUP BY GROUPING SETS (
-          (
-              pb.fiscal,
-              fy.fiscal_year,
-              pb.quarter,
-              pb.recovery_area,
-              po.portfolio_abbrev,
-              po.expense_authority,
-              cc.client,
-              cc.responsibility_centre,
-              cc.service_line,
-              cc.stob,
-              cc.project_code,
-              po.client,
-              po.responsibility,
-              po.service_line,
-              pb.stob,
-              po.cas_project_code,
-              p.project_number,
-              p.project_name,
-              p.id,
-              p.grp
-          ),
-          (
-              pb.fiscal,
-              fy.fiscal_year,
-              pb.quarter,
-              pb.recovery_area,
-              po.portfolio_abbrev,
-              po.expense_authority,
-              cc.client,
-              cc.responsibility_centre,
-              cc.service_line,
-              cc.stob,
-              cc.project_code,
-              p.project_number,
-              p.project_name,
-              p.id
-          )
-  )
-
-  ORDER BY fy.fiscal_year,  pb.quarter, po.portfolio_abbrev, p.project_number, CASE WHEN p.grp IS NULL THEN 0 ELSE 1 END`;
+          po.portfolio_abbrev,
+          p.project_number,
+          CASE
+            WHEN p.grp IS NULL THEN 0
+            ELSE 1
+          END 
+`;
 
 /**
  * Retrieves the data for various financial metrics based on the fiscal year.
