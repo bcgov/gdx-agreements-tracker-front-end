72d222484a7123412d97ed82be55c5c95a91f87e
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-05-28 21:50:56 -0700

    fix tab 15 report

--- backend/src/models/reports/Tab_15_rpt_P_QuarterlyBillingRequest.js
+++ backend/src/models/reports/Tab_15_rpt_P_QuarterlyBillingRequest.js
@@ -1,69 +1,176 @@
 const dbConnection = require("@database/databaseConnection");
 const { knex } = dbConnection();
-const { getProjectById } = require("@models/reports/useProject");
-const {
-  projectBudgetTable,
-  projectDeliverableTable,
-  clientCodingTable,
-  contactTable,
-  jvTable,
-} = require("@models/useDbTables");
 
 /**
- * Get array of deliverables with summed budget amounts.
+ * Retrieves the data for various financial metrics based on the fiscal year.
  *
- * @param   {int}   projectId The Project id for a specific project.
- * @param   {int}   fiscal    The fiscal for the project.
- * @param   {int}   quarter   The quarter for the fiscal of the project.
- * @returns {any[]}
+ * @param   {number | string | Array} fiscal - The fiscal year(s) to retrieve totals for.
+ * @returns {Promise}                        - A promise that resolves to the query result containing the totals for recoveries, expenses, net recoveries, and quarterly gross and net amounts.
  */
-const getDeliverableBudgets = (projectId, fiscal, quarter) => {
-  return knex(`${projectBudgetTable} as pb`)
-    .select("pd.deliverable_name", {
-      amount: knex.raw(`SUM(pb.q${quarter}_amount::numeric::float8)`),
-    })
-    .join(`${projectDeliverableTable} as pd`, "pb.project_deliverable_id", "pd.id")
-    .where("pd.project_id", projectId)
-    .andWhere("pb.fiscal", fiscal)
-    .groupBy("pd.id")
-    .orderBy("pd.deliverable_name", "ASC")
-    .having(knex.raw(`SUM(pb.q${quarter}_amount::numeric::float8)`), ">", 0);
+
+const getQuarterlySelectQuery = (quarter) => {
+  return knex
+    .select(
+      "id",
+      "project_deliverable_id",
+      "fiscal",
+      "client_coding_id",
+      "contract_id",
+      "notes",
+      "detail_amount",
+      "recovery_area",
+      "resource_type",
+      "stob",
+      { quarter },
+      `q${quarter}_recovered AS recovered`,
+      `q${quarter}_amount AS amount`
+    )
+    .from("data.project_budget");
 };
 
-/**
- * Get project's journal voucher data.
- *
- * @param   {int}   projectId The Project id for a specific project.
- * @param   {int}   fiscal    The fiscal for the project.
- * @param   {int}   quarter   The quarter for the fiscal of the project.
- * @returns {any[]}
- */
-const getJournalVoucher = (projectId, fiscal, quarter) => {
-  return knex(`${jvTable}`)
-    .select("*")
-    .where("project_id", projectId)
-    .andWhere("fiscal_year_id", fiscal)
-    .andWhere("quarter", quarter)
+const getDeliverableTotals = (fiscal, quarter, project) => {
+  return knex
+    .select(
+      knex.raw(
+        "SUM(CASE WHEN pb.recovered = true THEN pb.amount ELSE 0.00::money END) AS deliverable_totals"
+      )
+    )
+    .from(
+      knex
+        .union([
+          getQuarterlySelectQuery(1),
+          getQuarterlySelectQuery(2),
+          getQuarterlySelectQuery(3),
+          getQuarterlySelectQuery(4),
+        ])
+        .as("pb")
+    )
+    .innerJoin("data.project_deliverable AS pd", "pb.project_deliverable_id", "pd.id")
+    .where("pb.recovered", true)
+    .andWhere("pb.amount", "!=", knex.raw("0.00::money"))
+    .andWhere("pd.project_id", project)
+    .andWhere("pd.fiscal", fiscal)
+    .andWhere("pb.quarter", quarter)
     .first();
 };
 
-/**
- * Get project's client coding data.
- *
- * @param   {int}   projectId The Project id for a specific project.
- * @returns {any[]}
- */
-const getClientCoding = (projectId) => {
-  return knex(`${clientCodingTable} as cc`)
-    .select("cc.*", "c.*", { client_name: knex.raw("c.last_name || ', ' || c.first_name") })
-    .join(`${contactTable} as c`, "cc.contact_id", "c.id")
-    .where("cc.project_id", projectId)
-    .first();
+const queries = {
+  reportDeliverableTotals: (fiscal, quarter, project) =>
+    getDeliverableTotals(fiscal, quarter, project),
+  reportDeliverables: (fiscal, quarter, project) => {
+    return knex
+      .select([
+        "pd.deliverable_name",
+        knex.raw(
+          "SUM(CASE WHEN pb.recovered = true THEN pb.amount ELSE 0.00::money END) AS total_amount"
+        ),
+      ])
+      .from(
+        knex
+          .union([
+            getQuarterlySelectQuery(1),
+            getQuarterlySelectQuery(2),
+            getQuarterlySelectQuery(3),
+            getQuarterlySelectQuery(4),
+          ])
+          .as("pb")
+      )
+      .innerJoin("data.project_deliverable AS pd", "pb.project_deliverable_id", "pd.id")
+      .where("pb.recovered", true)
+      .andWhere("pb.amount", "!=", knex.raw("0.00::money"))
+      .andWhere("pd.project_id", project)
+      .andWhere("pd.fiscal", fiscal)
+      .andWhere("pb.quarter", quarter)
+      .groupBy(["pd.deliverable_name"])
+      .orderBy("pd.deliverable_name");
+  },
+  reportProject: (fiscal, quarter, project) => {
+    return knex
+      .select(
+        "p.project_number",
+        "p.project_name",
+        knex.raw(
+          "CASE WHEN p.project_status = 'Complete' THEN TRUE ELSE FALSE END AS is_completed"
+        ),
+        "cc.id AS client_coding_id",
+        "cc.project_id",
+        "fy.fiscal_year",
+        "pb.quarter",
+        "cc.client",
+        "cc.responsibility_centre",
+        "cc.service_line",
+        "cc.stob",
+        "cc.project_code",
+        knex.raw("COALESCE(c.first_name, '') || ' ' || COALESCE(c.last_name, '') AS full_name"),
+        "c.email",
+        "c.contact_phone",
+        "c.contact_title",
+        "cc.contact_id",
+        "jv.billed_date"
+      )
+      .from(
+        knex
+          .union([
+            getQuarterlySelectQuery(1),
+            getQuarterlySelectQuery(2),
+            getQuarterlySelectQuery(3),
+            getQuarterlySelectQuery(4),
+          ])
+          .as("pb")
+      )
+      .innerJoin("data.project_deliverable AS pd", "pb.project_deliverable_id", "pd.id")
+      .innerJoin("data.fiscal_year AS fy", "pd.fiscal", "fy.id")
+      .innerJoin("data.client_coding AS cc", "pb.client_coding_id", "cc.id")
+      .innerJoin("data.contact AS c", "cc.contact_id", "c.id")
+      .innerJoin("data.project AS p", "cc.project_id", "p.id")
+      .innerJoin("data.jv AS jv", "cc.id", "jv.client_coding_id")
+      .where("pb.recovered", true)
+      .andWhere("pb.amount", "!=", knex.raw("0.00::money"))
+      .andWhere("pd.project_id", project)
+      .andWhere("pd.fiscal", fiscal)
+      .andWhere("pb.quarter", quarter)
+      .groupBy([
+        "p.project_number",
+        "p.project_name",
+        "cc.id",
+        "cc.project_id",
+        "fy.fiscal_year",
+        "pb.quarter",
+        "c.first_name",
+        "c.last_name",
+        "cc.client",
+        "cc.responsibility_centre",
+        "cc.service_line",
+        "cc.stob",
+        "cc.project_code",
+        "c.email",
+        "c.contact_phone",
+        "c.contact_title",
+        "cc.contact_id",
+        "p.project_status",
+        "jv.billed_date",
+      ])
+      .first();
+  },
 };
 
 module.exports = {
-  getProjectById,
-  getDeliverableBudgets,
-  getJournalVoucher,
-  getClientCoding,
+  required: ["fiscal", "quarter", "project_number"],
+  getAll: async (query) => {
+    try {
+      const { fiscal, quarter, project } = query;
+
+      const [reportDeliverables, reportProject, reportDeliverableTotals] = await Promise.all([
+        queries.reportDeliverables(fiscal, quarter, project),
+        queries.reportProject(fiscal, quarter, project),
+        queries.reportDeliverableTotals(fiscal, quarter, project),
+      ]);
+
+      return { reportDeliverables, reportProject, reportDeliverableTotals };
+    } catch (error) {
+      console.error(error);
+
+      return null;
+    }
+  },
 };
