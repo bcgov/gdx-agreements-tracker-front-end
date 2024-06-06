e5dba17c31feb014a7986df14a3151279d76b6a3
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-06-06 10:46:16 -0700

    deleted tab 18 routes and controller

--- backend/src/models/reports/Tab_18_rpt_P_StatusSummary.js
+++ backend/src/models/reports/Tab_18_rpt_P_StatusSummary.js
@@ -1,17 +1,40 @@
+// Libs
+const { knex } = require("@database/databaseConnection")();
+const log = require("../../facilities/logging")(module.filename);
+const { getCurrentDate } = require("@controllers/reports/helpers");
+const { findMostRecentStatusById } = require("@models/projects");
 const {
-  getProjectById,
-  projectStatusReport,
-  getMilestones,
-  getStrategicAlignment,
-  getProjectStatuses,
-  getLessonsLearned,
-} = require("@models/reports/useProject");
+  changeRequestTable,
+  changeRequestTypeLookupTable,
+  changeRequestTypeTable,
+} = require("@models/useDbTables");
 
-module.exports = {
-  getProjectById,
-  projectStatusReport,
-  getMilestones,
-  getStrategicAlignment,
-  getProjectStatuses,
-  getLessonsLearned,
+const { dateFormatShortYear } = require("@helpers/standards");
+const queries = {};
+
+/**
+ * Retrieve and process data from queries to create a structured result object.
+ *
+ * @param   {object} options            - Options object containing fiscal year.
+ * @param   {string} options.fiscal     - The fiscal year to retrieve data for.
+ * @param   {string} options.quarter    - The fiscal year to retrieve data for.
+ * @param            options.project_id - The project id to filter on project id.  This param may be called project.
+ * @param            options.project    - The project id to filter on project id.  This param may be called project_id.
+ * @returns {object}                    - An object containing fiscal year, report, and report total.
+ */
+// add other parameters if needed, like quarter, portfolio, date etc.
+const getAll = async ({ project }) => {
+  const projectId = Number(project);
+  try {
+    const [] = await Promise.all([]);
+
+    return {};
+  } catch (error) {
+    log.error(error);
+    throw error;
+  }
 };
+
+// Export the functions to be used in controller.
+//  required can be fiscal, date, portfolio, etc.
+module.exports = { required: ["project"], getAll };
