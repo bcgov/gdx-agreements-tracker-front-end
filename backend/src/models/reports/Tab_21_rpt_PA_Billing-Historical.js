b2d03e1052a679b4157221e1b95089833d3bf6a0
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-06-11 08:04:57 -0700

    first model addition

--- backend/src/models/reports/Tab_21_rpt_PA_Billing-Historical.js
+++ backend/src/models/reports/Tab_21_rpt_PA_Billing-Historical.js
@@ -7,128 +7,100 @@ const { knex } = dbConnection();
  * @param   {string}            fiscal - Fiscal year value to filter the report.
  * @returns {Knex.QueryBuilder}        Knex query builder for fetching report data.
  */
-const baseQueries = {
-  projectRecoveryHistorical: knex("historical_projects")
-    .select({
-      project_number: "historical_projects.project_number",
-      project_name: "historical_projects.project_name",
-      total_project_budget: "historical_projects.total_project_budget",
-      fiscal_id: "fiscal_year.id",
-      budget_fiscal: "fiscal_year.fiscal_year",
-      q1: "historical_project_billing.q1",
-      q2: "historical_project_billing.q2",
-      q3: "historical_project_billing.q3",
-      q4: "historical_project_billing.q4",
-      total_recovered: knex.raw(
-        "cast( COALESCE(Q1, '0') + COALESCE(Q2, '0') + COALESCE(Q3, '0') + COALESCE(Q4, '0') as money)"
-      ),
-    })
-    .innerJoin("fiscal_year", "historical_projects.fiscal_year", "fiscal_year.id")
-    .innerJoin(
-      "historical_project_billing",
-      "historical_projects.project_number",
-      "historical_project_billing.project_number"
-    )
-    .groupBy(
-      "historical_projects.project_number",
-      "historical_projects.project_name",
-      "historical_projects.total_project_budget",
-      "fiscal_year.id",
-      "fiscal_year.fiscal_year",
-      "historical_project_billing.q1",
-      "historical_project_billing.q2",
-      "historical_project_billing.q3",
-      "historical_project_billing.q4"
-    ),
-
-  projectRecovery: knex("fiscal_year")
-    .select({
-      project_id: "project.id",
-      project_number: "project.project_number",
-      project_name: "project.project_name",
-      total_project_budget: "project.total_project_budget",
-      fiscal_id: "fiscal_year.id",
-      fiscal_year: "fiscal_year.fiscal_year",
-      q1: knex.raw(`sum(case when quarter = '1' then amount else null end)`),
-      q2: knex.raw(`sum(case when quarter = '2' then amount else null end)`),
-      q3: knex.raw(`sum(case when quarter = '3' then amount else null end)`),
-      q4: knex.raw(`sum(case when quarter = '4' then amount else null end)`),
-      total_recovered: knex.sum("jv.amount"),
-    })
-    .innerJoin("jv", "fiscal_year.id", "jv.fiscal_year_id")
-    .innerJoin("project", "jv.project_id", "project.id")
-    .groupBy(
-      "project.id",
-      "project.project_number",
-      "project.total_project_budget",
-      "fiscal_year.id",
-      "fiscal_year.fiscal_year"
-    ),
-};
-
-/**
- * Retrieves final historical recoveries for the report.
- *
- * @param   {number | string | Array} fiscal - The fiscal year(s) to retrieve totals for.
- * @returns {Knex.QueryBuilder}              Knex query builder for fetching report data.
- */
-const reportQueries = {
-  report: (fiscal) =>
-    knex
-      .select({
-        budget_fiscal: "project_recovery_historical.budget_fiscal",
-        project_number: "project_recovery_historical.project_number",
-        project_name: "project_recovery_historical.project_name",
-        total_project_budget: "project_recovery_historical.total_project_budget",
-        q1: "project_recovery_historical.q1",
-        q2: "project_recovery_historical.q2",
-        q3: "project_recovery_historical.q3",
-        q4: "project_recovery_historical.q4",
-        total_recovered: knex.raw(
-          "cast( COALESCE(Q1, '0') + COALESCE(Q2, '0') + COALESCE(Q3, '0') + COALESCE(Q4, '0') as money)"
-        ),
-      })
-      .from(baseQueries.projectRecoveryHistorical.as("project_recovery_historical"))
-      .where("fiscal_id", fiscal)
+const queries = {
+  billing: () => {
+    return knex
+      .with(
+        "historical_projects",
+        knex
+          .select(
+            "hp.project_number",
+            "hp.project_name",
+            "hp.total_project_budget",
+            "fy.fiscal_year as budget_fiscal",
+            "hpb.q1",
+            "hpb.q2",
+            "hpb.q3",
+            "hpb.q4",
+            knex.raw(
+              `COALESCE(hpb.q1, '0.00') + COALESCE(hpb.q2, '0.00') + COALESCE(hpb.q3, '0.00') + COALESCE(hpb.q4, '0.00') AS total_recovered`
+            )
+          )
+          .from("data.historical_projects as hp")
+          .innerJoin(
+            "data.historical_project_billing as hpb",
+            "hp.project_number",
+            "hpb.project_number"
+          )
+          .innerJoin("data.fiscal_year as fy", "hpb.fiscal_year", "fy.id")
+          .groupBy(
+            "hp.project_number",
+            "hp.project_name",
+            "hp.total_project_budget",
+            "fy.fiscal_year",
+            "hpb.q1",
+            "hpb.q2",
+            "hpb.q3",
+            "hpb.q4"
+          )
+      )
+      .with(
+        "current_projects",
+        knex
+          .select(
+            "p.project_number",
+            "p.project_name",
+            "p.total_project_budget",
+            "fy.fiscal_year",
+            knex.raw(`SUM(CASE WHEN jv.quarter = '1' THEN jv.amount ELSE NULL END) AS q1`),
+            knex.raw(`SUM(CASE WHEN jv.quarter = '2' THEN jv.amount ELSE NULL END) AS q2`),
+            knex.raw(`SUM(CASE WHEN jv.quarter = '3' THEN jv.amount ELSE NULL END) AS q3`),
+            knex.raw(`SUM(CASE WHEN jv.quarter = '4' THEN jv.amount ELSE NULL END) AS q4`),
+            knex.raw(`SUM(jv.amount) AS total_recovered`)
+          )
+          .from("data.project as p")
+          .innerJoin("data.jv as jv", "p.id", "jv.project_id")
+          .innerJoin("data.fiscal_year as fy", "jv.fiscal_year_id", "fy.id")
+          .groupBy("p.project_number", "p.project_name", "p.total_project_budget", "fy.fiscal_year")
+      )
       .unionAll([
         knex
-          .select({
-            budget_fiscal: "project_recovery.fiscal_year",
-            project_number: "project_recovery.project_number",
-            project_name: "project_recovery.project_name",
-            total_project_budget: "project_recovery.total_project_budget",
-            q1: "q1",
-            q2: "q2",
-            q3: "q3",
-            q4: "q4",
-            total_recovered: "total_recovered",
-          })
-          .from(baseQueries.projectRecovery.as("project_recovery"))
-          .where("fiscal_id", fiscal)
-          .orderBy("project_number"),
-      ]),
-
-  report_totals: (fiscal) =>
-    knex
-      .from(baseQueries.projectRecoveryHistorical.as("project_recovery_historical"))
-      .select({
-        q1_total: knex.sum("q1"),
-        q2_total: knex.sum("q2"),
-        q3_total: knex.sum("q3"),
-        q4_total: knex.sum("q4"),
-        grand_total: knex.sum("total_recovered"),
-      })
-      .where("fiscal_id", fiscal),
+          .select(
+            "project_number",
+            "project_name",
+            "total_project_budget",
+            "budget_fiscal",
+            "q1",
+            "q2",
+            "q3",
+            "q4",
+            knex.raw(
+              `COALESCE(q1, '0.00') + COALESCE(q2, '0.00') + COALESCE(q3, '0.00') + COALESCE(q4, '0.00') AS total_recovered`
+            )
+          )
+          .from("historical_projects"),
+        knex
+          .select(
+            "project_number",
+            "project_name",
+            "total_project_budget",
+            "fiscal_year",
+            "q1",
+            "q2",
+            "q3",
+            "q4",
+            "total_recovered"
+          )
+          .from("current_projects"),
+      ]);
+  },
 };
 
 module.exports = {
   required: ["fiscal"],
-  getAll: async ({ fiscal }) => {
-    const [report, report_totals] = await Promise.all([
-      reportQueries.report(fiscal),
-      reportQueries.report_totals(fiscal),
-    ]);
+  getAll: async () => {
+    const [reportBilling] = await Promise.all([queries.billing()]);
 
-    return { report, report_totals };
+    return { reportBilling };
   },
 };
